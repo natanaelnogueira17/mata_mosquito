fb08209a39b8e5056b4d7612c1e63d8bd2b2e456
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-24 16:43:43 -0300

    deixando o mosquito aparecendo de modo dinamico

--- jogo.js
+++ jogo.js
@@ -1,32 +1,37 @@
 var altura = 0
 var largura = 0
 function ajustatamanhoPalcoJogo(){
-     altura = window.innerHeight
+    altura = window.innerHeight
     largura = window.innerWidth
     console.log(altura, largura)
 }
 
 ajustatamanhoPalcoJogo()
-var posicaoX = Math.floor(Math.random() *largura) -90
-var posicaoY = Math.floor(Math.random() *altura) -90
-posicaoX = posicaoX< 0 ? 0 : posicaoX
-posicaoY = posicaoY < 0 ? 0 : posicaoY 
 
+function posicaoRandomica(){   
+    //remover o mosquito anterior 
+    if(document.getElementById('mosquito')){ //para verificar se existe o mosquito
+        document.getElementById('mosquito').remove() //remover o mosquito anterior
+    }
 
-
-console.log(posicaoY, posicaoX)
-
-//criando o elemento HTML
-var mosquito = document.createElement('img')
-mosquito.src = "imagens/mosca.png"
-mosquito.className = tamanhoAleatorio() +" " + ladoAleatorio()
-mosquito.style.left = posicaoX + 'px'
-mosquito.style.top = posicaoY + 'px'
-mosquito.style.position = 'absolute'
-document.body.appendChild(mosquito)
-
-
-
+    var posicaoX = Math.floor(Math.random() *largura) -90
+    var posicaoY = Math.floor(Math.random() *altura) -90
+    posicaoX = posicaoX< 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY    
+    
+    
+    console.log(posicaoY, posicaoX)
+    
+    //criando o elemento HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = "imagens/mosca.png"
+    mosquito.className = tamanhoAleatorio() +" " + ladoAleatorio()
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    document.body.appendChild(mosquito)
+} 
 
 function tamanhoAleatorio(){
     var classe = Math.floor(Math.random() * 3)
