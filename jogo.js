fe5ef3696164eac5bdf46027488235651ef76901
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-24 15:35:46 -0300

    initial commit

---
+++ jogo.js
@@ -0,0 +1,10 @@
+var altura = 0
+var largura = 0
+function ajustatamanhoPalcoJogo(){
+
+     altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(altura, largura)
+}
+
+ajustatamanhoPalcoJogo()
