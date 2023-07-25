bc793fb7bca7a42566a346858c0d9585f5977eb9
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-25 14:36:31 -0300

    ajustando o tempo de acordo com o nivel

--- jogo.js
+++ jogo.js
@@ -2,10 +2,21 @@ var altura = 0
 var largura = 0
 var vidas = 1
 var tempo = 30
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+if(nivel === 'normal' ){
+    criaMosquitoTempo = 1500
+}else if(nivel === 'dificil'){
+    criaMosquitoTempo = 1000
+}else if(nivel === 'hard'){
+    criaMosquitoTempo = 750
+}
+
 function ajustatamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
 }
 
 ajustatamanhoPalcoJogo()
