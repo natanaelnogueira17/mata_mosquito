7dbdb7b2bfab50e320c77dc94365af1d0ae9bc1c
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-25 13:43:00 -0300

    criando o cronometro e ajustando o background

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 30
 function ajustatamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
@@ -8,6 +9,18 @@ function ajustatamanhoPalcoJogo(){
 }
 
 ajustatamanhoPalcoJogo()
+
+var cronometro = setInterval(function(){
+    tempo-=1
+    if(tempo < 0){
+        clearInterval(cronometro)
+        clearInterval(criaMosquito)
+        alert("Champion!")
+    }else{
+
+        document.getElementById('cronometro').innerHTML = tempo
+    }
+}, 1000)
 
 function posicaoRandomica(){   
     //remover o mosquito anterior 
