cb03220c67a937b2753d50127ebdee58616b3d7b
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-25 12:45:49 -0300

    controlando as vidas

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 function ajustatamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
@@ -12,6 +13,12 @@ function posicaoRandomica(){
     //remover o mosquito anterior 
     if(document.getElementById('mosquito')){ //para verificar se existe o mosquito
         document.getElementById('mosquito').remove() //remover o mosquito anterior
+        if(vidas > 3){
+            alert('Fim de jogo, recarregue a página!')
+        }else{
+            document.getElementById('v' + vidas).src = "imagens/coracao_vazio.png"
+            vidas++
+        }
     }
 
     var posicaoX = Math.floor(Math.random() *largura) -90
@@ -30,6 +37,9 @@ function posicaoRandomica(){
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function(){
+        this.remove()
+    }
     document.body.appendChild(mosquito)
 } 
 
