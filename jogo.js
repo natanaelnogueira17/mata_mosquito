2b0187abb381ad6752414e3919aacfe5aee1d9ff
natanael nogueira <natanaelnogueira019@gmail.com>
2023-07-25 13:48:07 -0300

    criando a pagina de vitoria

--- jogo.js
+++ jogo.js
@@ -15,7 +15,7 @@ var cronometro = setInterval(function(){
     if(tempo < 0){
         clearInterval(cronometro)
         clearInterval(criaMosquito)
-        alert("Champion!")
+        window.location.href = "vitoria.html"
     }else{
 
         document.getElementById('cronometro').innerHTML = tempo
